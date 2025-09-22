d81f7ac7b62d13dfbfff9d7284c9546affa7e9bd
Nick Entin <nckentn@gmail.com>
2025-09-21 23:08:41 -0700

    Support interspersing items in addition to alignables (#24)

--- Paralayout/Distribution/HorizontalDistributionItem.swift
+++ Paralayout/Distribution/HorizontalDistributionItem.swift
@@ -167,6 +167,50 @@ public enum HorizontalDistributionItem: Sendable {
 
 // MARK: -
 
+extension Array where Element == HorizontalDistributionItem {
+    /// Return a distribution where the `interspersedItem` is inserted between each of the items in the receiver.
+    @MainActor
+    public func interspersed(with interspersedItem: HorizontalDistributionItem) -> [HorizontalDistributionItem] {
+        reduce([]) { partial, next in
+            if partial.isEmpty {
+                [next]
+            } else {
+                partial + [interspersedItem, next]
+            }
+        }
+    }
+
+    /// Return a distribution where the `interspersedItem` is inserted between each of the items in the receiver.
+    ///
+    /// For example, interspersing `16.fixed` into a distribution of
+    /// ```
+    /// [view1, view2, view3]
+    /// ```
+    /// gives a resulting distribution of
+    /// ```
+    /// [view1, 16.fixed, view2, 16.fixed, view3]
+    /// ```
+    @MainActor
+    public func interspersed(with interspersedItem: FixedSpacer) -> [HorizontalDistributionItem] {
+        interspersed(with: .fixed(interspersedItem.size.width))
+    }
+
+    /// Return a distribution where the `interspersedItem` is inserted between each of the items in the receiver.
+    ///
+    /// For example, interspersing `1.flexible` into a distribution of
+    /// ```
+    /// [view1, view2, view3]
+    /// ```
+    /// gives a resulting distribution of
+    /// ```
+    /// [view1, 1.flexible, view2, 1.flexible, view3]
+    /// ```
+    @MainActor
+    public func interspersed(with interspersedItem: FlexibleSpacer) -> [HorizontalDistributionItem] {
+        interspersed(with: .flexible(interspersedItem.weight))
+    }
+}
+
 extension Array where Element: Alignable {
     /// Return a distribution where the `interspersedItem` is inserted between each of the items in the receiver.
     @MainActor
